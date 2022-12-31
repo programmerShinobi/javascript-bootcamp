0018728570fcc51fe17d00c898351c0541e74d07
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2022-12-31 21:04:07 +0700

    Update lessons

--- all/06-b-02--array.js
+++ all/06-b-02--array.js
@@ -4,35 +4,63 @@
  *      yang digunakan untuk menyimpan sekumpulan nilai
  *      atau elemen yang terkait
  * Array terdapat beberapa methode : 
- * - array.push() :
- *   --->   Metode untuk menambah elemen baru ke akhir array
+ * ---> array.push() : Menambah elemen baru ke akhir array dan mengembalikan elemen tersebut
  * 
- * - array.pull() :
- *   --->   Metode untuk menghapus elemen terakhir dari array
+ * ---> array.pull() : Menghapus elemen terakhir dari array
  * 
- * - array.shift() : 
- *   --->   Metode untuk menghapus elemen pertama dari array
+ * ---> array.shift() : Menghapus elemen pertama dari array dan mengembalikan elemen tersebut
  * 
- * - array.unshift() :
- *   --->   Metode untuk menambah elemen baru ke awal array
- * - array.splice() :
- *   --->   Methode untuk menambah
- *          atau menghapus elemen di posisi tertentu dalam array
- * - array.slice() :
- *   --->   Method untuk mengambil sebagian dari array
-Array.concat(): menggabungkan dua atau lebih array menjadi satu array baru.
-Array.join(): menggabungkan semua elemen array menjadi sebuah string dengan pemisah yang ditentukan.
-Array.pop(): menghapus elemen terakhir dari array dan mengembalikan elemen tersebut.
-Array.push(): menambah elemen baru ke akhir array dan mengembalikan panjang array setelah elemen ditambahkan.
-Array.reverse(): membalikkan urutan elemen dalam array.
-Array.shift(): menghapus elemen pertama dari array dan mengembalikan elemen tersebut.
-Array.slice(): mengambil sebagian dari array dan mengembalikan sebuah array baru.
-Array.sort(): mengurutkan elemen dalam array.
-Array.splice(): menambah atau menghapus elemen dari array pada posisi tertentu.
-Array.unshift(): menambah elemen baru ke awal array dan mengembalikan panjang array setelah elemen ditambahkan.
+ * ---> array.unshift() : Menambah elemen baru ke awal array
  * 
+ * ---> array.splice() : Menambah atau menghapus elemen di posisi tertentu dalam array
  * 
+ * ---> array.slice() : Mengambil sebagian dari array dan mengembalikan array baru
  * 
+ * ---> array.concat() : Menggabungkan dua atau lebih array menjadi satu array baru
+ * 
+ * ---> array.join() : Menggabungkan semua elemen array menjadi sebuah string dengan pemisah yang ditentukan.
+ * 
+ * ---> array.reverse() : Membalikkan urutan elemen dalam array
+ * 
+ * ---> array.sort() : Mengurutkan elemen dalam array
+ * 
+ * ---> array.copyWithin()  : Menyalin elemen dari array ke posisi lain dalam array yang sama
+ * 
+ * ---> array.entries() : Mengembalikan iterator yang mengiterasi pasangan indeks-elemen dalam array
+ * 
+ * ---> array.every() : Memeriksa apakah semua elemen dalam array memenuhi kondisi yang ditentukan
+ * 
+ * ---> array.fill() : Mengisi semua elemen dalam array dengan nilai yang ditentukan
+ * 
+ * ---> array.filter() : Mengembalikan array baru yang hanya berisi elemen-elemen yang memenuhi kondisi yang ditentukan
+ * 
+ * ---> array.find() : Mencari elemen pertama dalam array yang memenuhi kondisi yang ditentukan
+ * 
+ * ---> array.findIndex() : Mencari indeks elemen pertama dalam array yang memenuhi kondisi yang ditentukan
+ * 
+ * ---> array.forEach() : Mengiterasi setiap elemen dalam array
+ * 
+ * ---> array.includes() : Memeriksa apakah array berisi elemen yang ditentukan
+ * 
+ * ---> array.indexOf() : Mencari indeks elemen pertama dalam array yang sama dengan elemen yang ditentukan
+ * 
+ * ---> array.isArray() : Memeriksa apakah suatu variabel merupakan array
+ * 
+ * ---> array.keys() : Mengembalikan iterator yang mengiterasi indeks-indeks dalam array
+ * 
+ * ---> array.lastIndexOf() : Mencari indeks elemen terakhir dalam array yang sama dengan elemen yang ditentukan
+ * 
+ * ---> array.map() : Mengembalikan array baru yang terdiri dari hasil dari masing-masing elemen array yang diubah sesuai dengan function yang ditentukan.
+ * 
+ * ---> array.reduce() : Mengembalikan hasil dari mengurangi semua elemen array menjadi satu nilai menggunakan function yang ditentukan
+ * 
+ * ---> array.reduceRight() : Sama dengan reduce, tapi mengurangi elemen dari kanan ke kiri
+ * 
+ * ---> array.some() : Memeriksa apakah ada setidaknya satu elemen dalam array yang memenuhi kondisi yang ditentukan
+ * 
+ * ---> array.toString() : Mengubah array menjadi string
+ * 
+ * ---> array.values() : Mengembalikan iterator yang mengiterasi elemen-elemen dalam array
  * 
  */
 
