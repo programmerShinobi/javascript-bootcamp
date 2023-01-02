92475bdfcfa48b670e4082cb6162acedadaefec0
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-03 03:14:22 +0700

    Update lessons

---
+++ all/06-b-03-function.js
@@ -0,0 +1,28 @@
+/**
+ * Function()
+ * ---> Block kode yang dapat dijalankan berkali-kali.
+ *      Function dapat menerima input berupa parameter
+ *      dan dapat mengembalikan nilai output
+ *      melalui return statement.
+ */
+
+function greet1(name) {
+    console.info("Hello, " + name + "!");
+}
+greet1("shinobi"); // Hello, shinobi!
+//---------------------------------------------------
+
+function greet2(name = "world") {
+    console.log("Hello, " + name + "!");
+}
+
+greet2(); // Hello, world!
+
+//---------------------------------------------------
+
+function add(a, b) {
+    return a + b;
+}
+
+const sum = add(1, 2); // sum akan bernilai 3
+console.info(sum); // 3 ---> didapat dari add(1+3)
